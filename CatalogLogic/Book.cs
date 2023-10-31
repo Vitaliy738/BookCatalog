3d9aae10ad86e5552823fd33dda035a3c8241a47
Vitaliy <smssmsdfgyts@gmail.com>
2023-10-31 21:41:35 +0300

    Catalog alpha v1.0.1

--- CatalogLogic/Book.cs
+++ CatalogLogic/Book.cs
@@ -2,9 +2,9 @@ namespace CatalogLogic;
 
 public class Book
 {
-    public string? Author;
-    public string? Name;
-    public string? IconPath;
+    public string? Author { get; set; }
+    public string? Name { get; set; }
+    public string? IconPath { get; set; }
 
     public Book() : this("Data is not available") {}
     public Book(string? author) : this(author, "Data is not available") {}
