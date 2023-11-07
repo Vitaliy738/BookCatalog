fbf078fbbe140bbdee0fea3a309f18b21238eea7
Vitaliy <smssmsdfgyts@gmail.com>
2023-11-07 12:01:54 +0200

    filter

--- Book catalog/MVVM/ViewModel/CatalogViewModel.cs
+++ Book catalog/MVVM/ViewModel/CatalogViewModel.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
+using System.Windows;
+using System.Windows.Input;
 using Book_catalog.Core;
 using CatalogLogic;
 
@@ -6,10 +10,82 @@ namespace Book_catalog.MVVM.ViewModel;
 
 public class CatalogViewModel : ObservableObject
 {
+    public RelayCommand NameFilterCommand { get; set; }
+    public RelayCommand AuthorFilterCommand { get; set; }
+    public RelayCommand MultiFilterCommand { get; set; }
+    
+    // Колекція усіх книжок католога
     public ObservableCollection<Book> Books { get; set; }
+    
+    public DataTable Bookss { get; set; }
+
+    // Значення для фільтрації за ім'ям
+    private string _nameSearch;
+    public string? NameSearch
+    {
+        get => _nameSearch;
+        set
+        {
+            if (value != null)
+            {
+                _nameSearch = value;
+                OnPropertyChanged("NameSearch");
+            }
+        }
+    }
+
+    // Значення для фільтрації за автором
+    private string _authorSearch;
+    public string? AuthorSearch
+    {
+        get => _authorSearch;
+        set
+        {
+            if (value != null)
+            {
+                _authorSearch = value;
+                OnPropertyChanged("AuthorSearch");
+            }
+        }
+    }
+
+    // Значення для філтрації за жанром
+    private string _genreSearch;
+    public string? GenreSearch
+    {
+        get => _genreSearch;
+        set
+        {
+            if (value != null)
+            {
+                _genreSearch = value;
+                OnPropertyChanged("GenreSearch");
+            }
+        }
+    }
+
+    // Значення для фільтрації за роком видання
+    private string _yearSearch;
+    public string? YearSearch
+    {
+        get => _yearSearch;
+        set
+        {
+            if (value != null)
+            {
+                _yearSearch = value;
+                OnPropertyChanged("YearSearch");
+            }
+        }
+    }
     
     public CatalogViewModel()
     {
+        List<string> list = new List<string>();
+        list.Add("1");
+        list.Add("2");
+        list.Add("3");
+        
         Books = new ObservableCollection<Book>
         {
             new Book(),
@@ -21,9 +97,59 @@ public class CatalogViewModel : ObservableObject
             new Book(),
             new Book(),
             new Book(),
-            new Book(),
-            new Book(),
-            new Book()
+            new Book("Petre", "Religion", "1456", list),
+            new Book("Vlad", "Echo", "2003"),
+            new Book("Vitaliy", "Oleg", "2023"),
+            new Book("Vitaliy", "Oleg - 2", "2023"),
+            new Book("Anna", "Silent"),
+            new Book("Vitaliy", "Danil voice")
         };
+
+        Bookss = new DataTable();
+
+        // Визначення структури DataTable
+        Bookss.Columns.Add("Author", typeof(string));
+        Bookss.Columns.Add("Name", typeof(string));
+        Bookss.Columns.Add("Year", typeof(string));
+        Bookss.Columns.Add("Genre", typeof(string));
+        Bookss.Columns.Add("IconPath", typeof(string));
+
+        // Додавання даних до DataTable
+        foreach (var book in Books)
+        {
+            Bookss.Rows.Add(book.Author, book.Name, book.Year, book.Genre, book.IconPath);
+        }
+
+        NameFilterCommand = new RelayCommand(_ =>
+        {
+            if (!string.IsNullOrEmpty(NameSearch))
+            {
+                Bookss.DefaultView.RowFilter = $"Name LIKE '%{NameSearch}%'";
+            }
+            else
+            {
+                Bookss.DefaultView.RowFilter = "";
+            }
+        });
+        
+        AuthorFilterCommand = new RelayCommand(_ =>
+        {
+            if (!string.IsNullOrEmpty(NameSearch))
+            {
+                Bookss.DefaultView.RowFilter = $"Author LIKE '%{AuthorSearch}%'";
+            }
+            else
+            {
+                Bookss.DefaultView.RowFilter = "";
+            }
+        });
+        
+        MultiFilterCommand = new RelayCommand(_ =>
+        {
+            Bookss.DefaultView.RowFilter = $"Author LIKE '%{AuthorSearch}%'" +
+                                           $"AND Name LIKE '%{NameSearch}%'" +
+                                           $"AND Year LIKE '%{YearSearch}%'"; 
+                                           //$"AND Genre LIKE '%{GenreSearch}%'";
+        });
     }
 }
--- CatalogLogic/Book.cs
+++ CatalogLogic/Book.cs
@@ -4,16 +4,21 @@ public class Book
 {
     public string? Author { get; set; }
     public string? Name { get; set; }
+    public string? Year { get; set; }
+    public List<string>? Genre { get; set; }
     public string? IconPath { get; set; }
 
     public Book() : this("Data is not available") {}
-    public Book(string? author) : this(author, "Data is not available") {}
-    public Book(string? author, string? name) : this(author, name, 
-        "C:\\Users\\Asus\\RiderProjects\\Book catalog\\Book catalog\\Icons\\BookIcon.png") {}
-    public Book(string? author, string? name, string? iconPath)
+    public Book(string author) : this(author, "Data is not available") {}
+    public Book(string author, string name) : this(author, name, "NONE") {}
+    public Book(string author, string name, string year) : this(author, name, year, new List<string>()) {}
+    public Book(string author, string name, string year, List<string> genre) : this(author, name, year, genre, "C:\\Users\\Asus\\RiderProjects\\Book catalog\\Book catalog\\Icons\\BookIcon.png") {}
+    public Book(string author, string name, string year, List<string> genre, string iconPath)
     {
         Author = author;
         Name = name;
+        Year = year;
+        Genre = genre;
         IconPath = iconPath;
     }
 }
